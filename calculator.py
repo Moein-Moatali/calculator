fe03cee687146c6392792e08d9b0b6faddbc5a7e
Moein-Moatali <moin.1384.1384.1384@gmail.com>
2023-05-17 15:04:14 +0330

    calculator

---
+++ calculator.py
@@ -0,0 +1,11 @@
+number1=int(input('please enter a number:'))
+number2=int(input('please enter a number:'))
+var=input("char:")
+if var=="+":
+    print(number1+number2)
+elif var=="*":
+    print(number1*number2)
+elif var=="-":
+    print(number1-number2)
+elif var=="/":
+    print(number1/number2)
